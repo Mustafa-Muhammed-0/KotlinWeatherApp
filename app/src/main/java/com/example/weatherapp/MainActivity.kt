bc9f53b27c433354fdae2f7a0f2f5160283640b6
Mustafa Muhammed <mustafa.abd.al.qaadir@gmail.com>
2024-05-15 20:34:08 +0300

    An update but not much cuz i've been really busy lately

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.weatherapp
 
+import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,37 +16,35 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.weatherapp.ui.theme.WeatherAppTheme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import org.json.JSONObject
 
 class MainActivity : ComponentActivity() {
+    var weather_url1 = ""
+    var api_id1 = "a0a1dcd668554fe099a9d84e74ff3a03"
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            WeatherAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+        var WeatherURL:String = "http://api.weatherbit.io/v2.0/current"
+        var listVi: ListView = findViewById(R.id.MainListView)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        Log.e("lat", WeatherURL)
+        var addbtn:Button = findViewById(R.id.addButton)
+
+        addbtn.setOnClickListener {
+            Log.e("lat", "onClick")
+
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherAppTheme {
-        Greeting("Android")
     }
-}
+
